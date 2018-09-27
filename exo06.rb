62c213f541bdd6fbe75979d13ec27af72d25a650
malo2k <43620886+malo2k@users.noreply.github.com>
2018-09-27 19:48:10 +0200

    Update exo06.rb

--- exo06.rb
+++ exo06.rb
@@ -3,7 +3,7 @@ number_of_days_worked_per_week = 5
 number_of_weeks_in_THP = 11
 
 puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
-# l'opération entre crochets #{} correspond au produit des valeurs pour chaque variables définies précédemment#
+# l'opération entre crochets #{} correspond au produit des valeurs pour chaque variables définies précédemment
 puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
-# la variable "number_of_minutes_in_an_hour" n'a pas été définie précédemment donc elle est nulle et ne s'applique pas#
+# la variable "number_of_minutes_in_an_hour" n'a pas été définie précédemment donc elle est nulle et ne s'applique pas
  
