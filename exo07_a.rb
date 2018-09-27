66d4d768834cddb877a123b5d20a41a8e8576ec6
malo2k <43620886+malo2k@users.noreply.github.com>
2018-09-27 19:52:01 +0200

    Update exo07_a.rb

--- exo07_a.rb
+++ exo07_a.rb
@@ -1,4 +1,4 @@
 puts "Bonjour, c'est quoi ton blase ?"
 user_name = gets.chomp
 puts user_name
-# gets.chomp correspond à une variable 
+# gets.chomp correspond à une variable. Après appui sur la trouche entrée, elle laisse un espace vide permettant un temps de réponse
