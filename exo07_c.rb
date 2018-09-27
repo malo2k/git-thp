43400767b7473780d9e23db6748a729a4585ed85
malo2k <malo2k@laposte.net>
2018-09-27 19:08:42 +0200

    exercice7

---
+++ exo07_c.rb
@@ -0,0 +1,5 @@
+user_name = gets.chomp
+puts user_name
+#le 1er programme publie uniquement la première phrase en attendant une donnée pour gets.chomp
+#le 2nd avec prints annule apparemment le puts
+#le 3ème est la variable uniquement, qui reste vide mais crée un espace pour une eventuelle donnée
