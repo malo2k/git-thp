43db5b1825cf015ae9ef5048e54200ac38b7036f
malo2k <malo2k@laposte.net>
2018-09-28 15:44:45 +0200

    exercice18

--- exo18.rb
+++ exo18.rb
@@ -1,4 +1,8 @@
 email_list = []
 50.times do |i|
-    email_list << "malo#{i+1}@email.com"
+  if i < 9
+    email_list << "malo0#{i+1}@email.fr"
+else
+  email_list << "malo#{i+1}@email.fr"
+end
 end
