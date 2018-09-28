f5d7ef20628faa0626e354255970d9bfd826ac8b
malo2k <malo2k@laposte.net>
2018-09-28 17:18:04 +0200

    exercice pyramidale

--- pyramide.rb
+++ pyramide.rb
@@ -1,27 +1,17 @@
-puts 'Bon donc tu veux construire une pyramide ? de combien d\'étages ?'
-print '> '
-nbFloor = gets.chomp.to_i
+puts 'Salut, rebienvenu dans ma super pyramide version2.0! 
+Combien d\'étages veux-tu ?'
+puts '> '
+n = gets.chomp.to_i
 
-if nbFloor.to_i <= 1
-	puts "C'est pas une pyramide ça !"
-
-if 1.is_a? Integer
-	puts "Et fait on te demande le nombre d\'étages"
-	
-end
-
-else
-
-nbFloor.downto(1) do |suite|
-  (suite - 1).times do
+n.downto(1) do |i|
+  (i - 1).times do
     print ' '
   end
 
-  (nbFloor - suite + 1).times do
+  (n - i + 1).times do
     print '#'
   end
 
 puts ''
 
-end
 end
